f725159e9f59751d12d74890a278a3c7ff2ebc17
akwl1208 <104331415+akwl1208@users.noreply.github.com>
2022-04-25 13:57:22 +0900

    자바 1일차: hello world 출력 예제

---
+++ java/src/day1/Ex1_Hello.java
@@ -0,0 +1,30 @@
+package day1;
+
+public class Ex1_Hello {
+	//콘솔 응용 프로그램이 실행되기 위해서 main이라는 (메서드)가 필요하다
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+		
+		/* 예제 1) 주석을 다는 방법
+		 * println(문자열) : 문자열을 콘솔에 출력한 후 줄바꿈함
+		 * print(문자열) : 문자열을 콘솔에 출력
+		 */
+		System.out.print("Hello world!");
+		System.out.println("Hello world!");
+		System.out.print("Hello world!");
+		
+		/*자주하는 에러
+		 * 가로 실수 : 가로 갯수 체크
+		 * 메인 밖에 코드 생성 : 들여쓰기 잘하기 (tab키) 들여쓰기 취소 (shift+tab)
+		 */
+		
+		
+
+		
+
+		
+	}
+
+}
