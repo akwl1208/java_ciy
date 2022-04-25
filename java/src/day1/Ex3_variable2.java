7ba1e402f56e207359aade130387a69098c1c61b
akwl1208 <104331415+akwl1208@users.noreply.github.com>
2022-04-25 16:13:17 +0900

    자바 1일차: 변수를 초기화해야 하는 이유

---
+++ java/src/day1/Ex3_variable2.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class Ex3_variable2 {
+
+	public static void main(String[] args) {
+		/* 변수는 읽기를 하려면 초기화가 되어있어야 한다
+		 * 초기화되어 있지 않은 변수를 읽어오려고 하면 에러 발생
+		 
+			int num;
+			System.out.println(num);
+		*/
+		
+		int num;
+		num =10;
+		System.out.println(num);
+	}
+
+}
