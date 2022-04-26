faa6dec4b1e1b4c04ffdcce9756e4c608221bcab
akwl1208 <104331415+akwl1208@users.noreply.github.com>
2022-04-26 12:07:25 +0900

    자바 2일차: 부호연산자

---
+++ java/src/day2/Ex1_sign.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class Ex1_sign {
+
+	public static void main(String[] args) {
+		//<부호연산자>
+		//정수형 변수 num1을 선언 후 1로 초기화
+		int num1 = 1;
+		System.out.println(num1);
+		//'-'는 음수 양수 교체 
+		System.out.println(-num1);
+
+	}
+
+}
