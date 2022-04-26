46dff0e0e6d47393ce3872ad13de52f8cefc091a
akwl1208 <104331415+akwl1208@users.noreply.github.com>
2022-04-26 14:02:35 +0900

    오타 수정

--- java/src/day2/Ex7_condition.java
+++ java/src/day2/Ex7_condition.java
@@ -7,7 +7,7 @@ public class Ex7_condition {
 		 * (조건식) ?  참일 때 값 : 거짓일 때 값
 		 */
 		
-		//예제) 성적이 A학점이면 A학점으로 축력하고, 아니면 A학점이 아님으로 출력
+		//예제) 성적이 A학점이면 A학점으로 출력하고, 아니면 A학점이 아님으로 출력
 		int score1 = 90;
 		String isA1 = (score1 >= 90 && score1 <= 100)? "A학점" : "A학점이 아님";
 		System.out.println(score1 + "점은 " + isA1); //A학점
