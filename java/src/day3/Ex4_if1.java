78b56f9b849b536675eb1d52313a8b98bfff71b5
akwl1208 <104331415+akwl1208@users.noreply.github.com>
2022-04-27 23:26:41 +0900

    자바 3일차: if문 예제

--- java/src/day3/Ex4_if1.java
+++ java/src/day3/Ex4_if1.java
@@ -7,6 +7,11 @@ public class Ex4_if1 {
 	public static void main(String[] args) {
 		// 예제) 두 정수와 산술 연산자를 입력받아 연산 결과를 출력하는 코드를 작성하세시오
 		
+		/*나누기 실수 주의
+		int res = (int)(1 / 2.0) ;
+		double res2 = 1 /2 ; //0.0 나옴 -> 이미 0이 나와서 0을 실수로 표현
+		*/
+		
 		//<내가 푼거>
 		
 		Scanner scan = new Scanner(System.in);
@@ -40,10 +45,6 @@ public class Ex4_if1 {
 			System.out.println("잘못된 연산자입니다");
 		}
 		
-		/*나누기 실수
-		int res = (int)(1 / 2.0) ;
-		double res2 = 1 /2 ; //0.0 나옴 -> 이미 0이 나와서 0을 실수로 표현
-		*/
 		
 		scan.close();
 	}
