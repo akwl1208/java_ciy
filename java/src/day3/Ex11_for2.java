3a13367d860ca45281dbbe9036bac6205b3e05ed
akwl1208 <104331415+akwl1208@users.noreply.github.com>
2022-04-27 16:53:06 +0900

    자바 3일차: for 예제_숫자 출력

---
+++ java/src/day3/Ex11_for2.java
@@ -0,0 +1,28 @@
+package day3;
+
+public class Ex11_for2 {
+
+	public static void main(String[] args) {
+		// 예제) 1부터 5까지 반복문을 이용하여 출력하는 코드를 작성하세요
+		
+		
+		//<내가 푼거>
+		/*출력모습
+		 * 1
+		 * 2
+		 * 3
+		 * 4
+		 * 5
+		 * i= n 이면 n이 출력 -> i를 출력하면 됨
+		 */
+		int i = 1;
+		
+		for(i=1 ; i<=5 ; i++) {
+			System.out.println(i);
+		}
+			
+				
+
+	}
+
+}
