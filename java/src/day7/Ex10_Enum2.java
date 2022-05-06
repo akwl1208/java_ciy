c34c7ffc0edc47399a9a7a901790738a8f1ff7e1
akwl1208 <104331415+akwl1208@users.noreply.github.com>
2022-05-06 15:12:20 +0900

    오타 수정

--- java/src/day7/Ex10_Enum2.java
+++ java/src/day7/Ex10_Enum2.java
@@ -6,7 +6,7 @@ public class Ex10_Enum2 {
 
 	public static void main(String[] args) {
 		//<열거 타입 변수>
-		//예제)두 정수와 산술 연산을 위한 문자열을 입력했을 때 산수 령ㄴ산 결과를 출력
+		//예제)두 정수와 산술 연산을 위한 문자열을 입력했을 때 산수연산 결과를 출력
 		Scanner scan = new Scanner(System.in);
 		System.out.print("두 정수를 입력하세요 : ");
 		int num1 = scan.nextInt();
