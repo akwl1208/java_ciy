ac2b9a2ccc751e213efc74db5eecb5e530ae0e92
akwl1208 <104331415+akwl1208@users.noreply.github.com>
2022-05-12 09:23:07 +0900

    자바 12일차_예제5_금호타이어_타이어 자식클래스

---
+++ java/src/day12/KumhoTire.java
@@ -0,0 +1,13 @@
+package day12;
+
+public class KumhoTire extends Tire {
+	public final static String COMPANY = "금호타이어";
+	//생성자
+	public KumhoTire(int state, int pressure, String position) {
+		super(state, pressure, position);
+	}
+	//기본 생성자
+	public KumhoTire() {
+		//super();
+	}
+}
