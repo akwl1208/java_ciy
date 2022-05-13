a1b16d09bd24d873724f5e29b312d1e174d1dbd4
akwl1208 <104331415+akwl1208@users.noreply.github.com>
2022-05-14 00:01:24 +0900

    12일차 복습: 제품 클래스_마트 예제

---
+++ java/src/review/Review_Product_day12.java
@@ -0,0 +1,40 @@
+package review;
+
+public abstract class Review_Product_day12 {
+	//필드: 제품명, 수량, 가격
+	private String name;
+	private int amount;
+	private int price;
+	
+	//생성자
+	public Review_Product_day12(String name, int amount, int price) {		
+		this.name = name;
+		this.amount = amount;
+		this.price = price;		
+	}
+	
+	//기능1) 제품 출력
+	public abstract void print();
+	
+	//getter/setter
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	
+}
