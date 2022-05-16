57fda81d92a634f3e682b157da41d34107a5ef60
akwl1208 <104331415+akwl1208@users.noreply.github.com>
2022-05-16 21:38:52 +0900

    자바 15일차: 사용자 정의 예외

---
+++ java/src/day15/Ex16_Exception11_Test2.java
@@ -0,0 +1,24 @@
+package day15;
+
+public class Ex16_Exception11_Test2 {
+
+	public static void main(String[] args) {
+		// <사용자 정의 예외>
+		try {
+			//throw new UserException();
+			throw new UserException("회원 관리 예외");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+}
+//사용자 정의 예외 클래스 
+class UserException extends Exception{
+	public UserException() {
+		super("회원 관리 예외");
+	}
+	public UserException(String message) {
+		super(message);
+	}
+}
